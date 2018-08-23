62be09fa12b962574823b4ee72f20fbad47e96dd
perryja <perryja@umail.iu.edu>
2018-08-23 09:09:36 -0700

    nothing

--- src/main/java/com/revature/test/TestSVPLogin.java
+++ src/main/java/com/revature/test/TestSVPLogin.java
@@ -4,7 +4,10 @@ import java.io.File;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class TestSVPLogin {
@@ -14,17 +17,18 @@ public class TestSVPLogin {
 	public void testSVPLogin() {
 		openChrome();
 		wd.get("https://assignforce-client.cfapps.io/login");
-		wd.findElement(By.name("email")).sendKeys("svp@revature.com");
-		wd.findElement(By.name("password")).sendKeys("p@$$w0rd");
-		wd.findElement(By.name("submit")).click();
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Assert.assertEquals(wd.getCurrentUrl(), "https://assignforce-client.cfapps.io/overview");
-	}
+		WebElement email = (new WebDriverWait(wd, 10)).
+				until(ExpectedConditions.elementToBeClickable(By.name("email")));
+		WebElement pass = (new WebDriverWait(wd, 10)).
+				until(ExpectedConditions.elementToBeClickable(By.name("password")));
+		WebElement submit = (new WebDriverWait(wd, 10)).
+				until(ExpectedConditions.elementToBeClickable(By.name("submit")));
+		email.sendKeys("svp@revature.com");
+		pass.sendKeys("p@$$w0rd");
+		submit.click();
+		(new WebDriverWait(wd, 10)).
+				until(ExpectedConditions.urlMatches("https://assignforce-client.cfapps.io/overview"));
+		Assert.assertEquals(wd.getCurrentUrl(), "https://assignforce-client.cfapps.io/overview");	}
 
 	public static void openChrome() {
 		  File f = new File("src/main/resources/chromedriver.exe");
